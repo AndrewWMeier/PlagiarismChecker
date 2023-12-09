eac3ad5177d427654a28748fb4e4bcb7d8f09816
Gauravjot Garaya <gauravjot@outlook.com>
2023-12-09 12:53:02 -0800

    frontend complete

---
+++ README copy.md
@@ -0,0 +1,36 @@
+# PlagiarismChecker
+
+A plagiarism checker for a final project
+
+## How to run
+
+1. Clone this repository and make a virtual environment.
+
+    ```bash
+    python3 -m venv .venv
+    source ./.venv/bin/activate
+    ```
+
+2. Install the required packages.
+
+    ```bash
+    python -m pip install -r requirements.txt
+    ```
+
+3. Start the webserver.
+
+    ```bash
+    python server.py
+    ```
+
+4. Go to [http://127.0.0.1:5000](http://127.0.0.1:5000) to access the application.
+
+## Sccreenshots
+
+### Homepage
+
+![Homepage](screenshots/homepage.png)
+
+### Results
+
+![Results](screenshots/results.png)
--- main.py
+++ main.py
@@ -7,53 +7,63 @@ import nltk
 from Source import Source
 from Comparison import Comparison
 
-#extracts paragraphs from website urls, this is called by the get_google_results function
+
+nltk.download('punkt')
+
+
+# extracts paragraphs from website urls, this is called by the get_google_results function
 def get_website_content(url):
     try:
         print(f"\nFetching {url}...")
         response = requests.get(url)
         soup = BeautifulSoup(response.text, 'html.parser')
 
-        #extract text from paragraphs
+        # extract text from paragraphs
         paragraphs = soup.find_all('p')
-        text_content = ' '.join([paragraph.get_text() for paragraph in paragraphs])
+        text_content = ' '.join([paragraph.get_text()
+                                for paragraph in paragraphs])
 
         return text_content
     except Exception as e:
         print(f"An error occurred while fetching content: {e}")
         return None
-    
-#searches google for the query and returns objects containing clean sentences from the sources and the url of the first 3 results
+
+
+# searches google for the query and returns objects containing clean sentences from the sources and the url of the first 3 results
 def get_google_results(query, num_results=3):
     try:
         search_results = list(search(query, num_results=num_results))
-        #initialize the array to store the Source objects
+        # initialize the array to store the Source objects
         all_sources = []
-        for i, result in enumerate(search_results[:num_results], start=1):  #limit to num_results
+        # limit to num_results
+        for i, result in enumerate(search_results[:num_results], start=1):
             # print(f"Result {i}:\n{result}")
             content = get_website_content(result)
             if content:
-                #clean up the content and create a source object
+                # clean up the content and create a source object
                 cleaned_content = data_cleanup(content)
                 source = Source(cleaned_content, result)
                 all_sources.append(source)
                 source.set_url(result)
         return all_sources
-    
+
     except Exception as e:
         print(f"An error occurred: {e}")
 
-#data cleanup function 
+
+# data cleanup function
 def data_cleanup(text):
-    #to lowercase and remove unnecessary spacing
+    # to lowercase and remove unnecessary spacing
     text = text.lower()
     text = text.replace("\n", "")
     # Use NLTK to tokenize the text into sentences
     sentences = nltk.sent_tokenize(text)
     return sentences
 
-#compare the user query with the text samples
+
+# compare the user query with the text samples
 def compare_sentences(user_query, sources):
+    result = []
     for source in sources:
         for sentence_sample in source.get_sentences():
             for sentence_user in user_query:
@@ -62,25 +72,36 @@ def compare_sentences(user_query, sources):
                 # print(f"User Sentence: {sentence_user}")
                 # print(f"Sample Sentence: {sentence_sample}")
                 # print("-----------------------------------------------------------------")
-                comparison = Comparison(sentence_user, sentence_sample, None, source.get_url())
-                if(comparison.similarity > 0.5):
+                comparison = Comparison(
+                    sentence_user, sentence_sample, None, source.get_url())
+
+                if (__name__ == '__main__'):
                     print(f"\nSimilarity score: {comparison.similarity}")
                     print(f"User Sentence: {sentence_user}")
                     print(f"Sample Sentence: {sentence_sample}")
                     print(f"Source URL: {comparison.url}")
-                    print("-----------------------------------------------------------------\n")
+                    print(
+                        "-----------------------------------------------------------------\n")
+                else:
+                    # return as json object
+                    result.append(dict(
+                        similarity=comparison.similarity,
+                        user_sentence=sentence_user,
+                        sample_sentence=sentence_sample,
+                        source_url=comparison.url
+                    ))
 
-#get user input and create a list of text samples from google
-user_query = input("Enter text to analyze:")
-text_samples = get_google_results(user_query)
-
-#clean up the user query
-user_query = data_cleanup(user_query)
-
-#compare the user query with the text samples
-compare_sentences(user_query, text_samples)
+    if (__name__ != '__main__'):
+        return result
 
 
+if __name__ == '__main__':
+    # get user input and create a list of text samples from google
+    user_query = input("Enter text to analyze:")
+    text_samples = get_google_results(user_query)
 
+    # clean up the user query
+    user_query = data_cleanup(user_query)
 
-
+    # compare the user query with the text samples
+    compare_sentences(user_query, text_samples)
