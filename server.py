45086a0370cafc1fd26fb8483b108bdc0ecc6021
Gauravjot Garaya <gauravjot@outlook.com>
2023-12-09 13:12:29 -0800

    Frontend Added (#1)
    * frontend complete
    
    * merged documentation files
    
    * minor readme changes and threshold changes
    
    * updated screenshot
    
    * made threshold limit changes

---
+++ server.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, request, redirect
+from main import get_google_results, data_cleanup, compare_sentences
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/check', methods=['POST'])
+def check():
+    # get text from form
+    text = request.form['text']
+    google_results = get_google_results(text)
+    # clean up the text
+    cleaned_text = data_cleanup(text)
+    # compare the sentences
+    comparison = compare_sentences(cleaned_text, google_results)
+
+    return render_template(
+        'check.html',
+        comparison=comparison,
+    )
+
+
+@app.route('/check', methods=['GET'])
+def redirectToHome():
+    return redirect("/", code=302)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
